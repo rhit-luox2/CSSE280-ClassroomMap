d45108cf664dec67591d4a9cd9375282560c57ac
rhit-zhuy17 <zhuy17@rose-hulman.edu>
2023-10-30 12:38:02 -0400

    login page setup

---
+++ Public/scripts/main.js
@@ -0,0 +1,26 @@
+var rhit = rhit || {};
+
+
+
+rhit.startFirebaseUI = function () {
+	var uiConfig = {
+		signInSuccessUrl: '/',
+		signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+			firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+		],
+	};
+	const ui = new firebaseui.auth.AuthUI(firebase.auth());
+	ui.start('#firebaseui-auth-container', uiConfig);
+};
+
+/* Main */
+/** function and class syntax examples */
+rhit.main = function () {
+	console.log("Ready");
+    rhit.startFirebaseUI();
+};
+
+rhit.main();
