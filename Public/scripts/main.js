743277de3c2ba9690af6d980f61c4c49dab52ee0
rhit-zhuy17 <zhuy17@rose-hulman.edu>
2023-10-30 13:42:13 -0400

    update

--- Public/scripts/main.js
+++ Public/scripts/main.js
@@ -1,4 +1,90 @@
 var rhit = rhit || {};
+rhit.fbAuthManager = null;
+
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
+
+rhit.MainPageController = class {
+    constructor() {
+
+    }
+}
+
+rhit.LoginPageController = class {
+	constructor() {
+		document.querySelector("#rosefireButton").onclick = (event) => {
+			rhit.fbAuthManager.signIn();
+		};
+	}
+}
+
+rhit.FbAuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+	signIn() {
+		console.log("Signing in using rosefire")
+		Rosefire.signIn("44f3ef77-0538-446a-9cb7-e43d984c3b7f", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				if (errorCode === 'auth/invalid-custom-token') {
+					alert('The token you provided is not valid.');
+				} else {
+					console.error("Custom auth error", errorCode, errorMessage);
+				}
+			});
+		});
+	}
+	signOut() {
+		firebase.auth().signOut().catch((error) => {
+			console.log("Sign out error");
+		});
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+	get uid() {
+		return this._user.uid;
+	}
+}
+
+rhit.checkForRedirects = function () {
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/search.html";
+	}
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
+	}
+};
+
+rhit.initializePage = function () {
+	if (document.querySelector("#mainPage")) {
+		console.log("On the main search page");
+		new rhit.MainPageController();
+	}
+
+	if (document.querySelector("#loginPage")) {
+		console.log("You are on the login Page.");
+		new rhit.LoginPageController();
+	}
+};
 
 rhit.startFirebaseUI = function () {
 	var uiConfig = {
