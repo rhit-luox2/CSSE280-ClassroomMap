e00335df117c94f11163fe17b11590592d87ebc7
rhit-luox2 <luox2@rose-hulman.edu>
2023-11-05 16:04:12 -0500

    mapper

---
+++ Public/scripts/mapper.js
@@ -0,0 +1,33 @@
+const classrooms = {};
+
+window.onload = function() {
+    const svgObject = document.getElementById('floorPlan');
+    svgObject.addEventListener('load', function() {
+        const svgDoc = svgObject.contentDocument;
+        svgDoc.addEventListener('click', (event) => {
+            const className = prompt("Enter the classroom name/number:");
+            if (className) {
+                classrooms[className] = {
+                    x: event.clientX,
+                    y: event.clientY
+                };
+                console.log('Stored:', className, 'at', classrooms[className]);
+            }
+        });
+    });
+};
+
+function loadSVG() {
+    const selectedFloor = document.getElementById('floorSelect').value;
+    document.getElementById('floorPlan').setAttribute('data', 'FloorPlan/' + selectedFloor);
+}
+
+function exportData() {
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(classrooms));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "classrooms.json");
+    document.body.appendChild(downloadAnchorNode); // Required for Firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
